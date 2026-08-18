fd4d329fddc26b07ea458544e19887ec705ab3d1
flipsewtf <mournstera@proton.me>
2026-08-18 23:58:40 +0200

    md to mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,6 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
-import { unified } from '@astrojs/markdown-remark';
-import rehypeFigure from '@microflash/rehype-figure';
 
 export default defineConfig({
     site: 'https://flipse.wtf',
@@ -12,13 +10,6 @@ export default defineConfig({
 
     devToolbar: {
         enabled: false,
-    },
-
-    markdown: {
-        syntaxHighlight: false,
-        processor: unified({
-            rehypePlugins: [rehypeFigure],
-        }),
     },
 
     vite: {
